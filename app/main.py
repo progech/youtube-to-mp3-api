dfad382d3b8f1e4a1ffec0f9c14b306fda3ab86b
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-05-06 16:04:13 +0500

    url to dom

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ origins = [
     # "http://localhost:3000",
     # "https://yourdomain.com",
     # "https://www.yourdomain.com",
-    "https://ytguru.vercel.app/"
+    "https://ytguru.vercel.app"
 ]
 
 app.add_middleware(
